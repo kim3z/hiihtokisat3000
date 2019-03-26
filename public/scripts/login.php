129aeb5cde9be4af222fa974c2489d1758e4916d
kim3z <kim.lehtinen@hotmail.com>
2019-03-26 21:03:12 +0200

    Working on project structure for simplicity

--- public/scripts/login.php
+++ public/scripts/login.php
@@ -4,19 +4,16 @@
   * @author Kim Lehtinen <kim.lehtinen@student.uwasa.fi>
   */
 
-include_once './Database/DB.php';
-include_once './Database/Models/User.php';
+include_once '../kantayhteys.php';
+include_once './classes/User.php';
 
-$db = new DB();
-$dbConn = $db->newConnection();
-
-$user = new User($dbConn);
+$user = new User($conn);
 
 $user->username = isset($_POST['username']) ? $_POST['username'] : die();
 $user->password = isset($_POST['password']) ? $_POST['password'] : die();
 
 if ($user->login()) {
-  echo 'true';
+  header('Location: sovellus/index.php');
 } else {
   echo 'false';
 }
