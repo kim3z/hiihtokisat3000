3584e688669e33337fa6a4e092e83e5855d71df2
Kim Lehtinen <kim.lehtinen@hotmail.com>
2019-04-09 08:46:43 +0300

    Update tulosseuranta_kisa_sarja_sivu.php

--- tulosseuranta_kisa_sarja_sivu.php
+++ tulosseuranta_kisa_sarja_sivu.php
@@ -3,6 +3,10 @@
     require_once './classes/Osallistuminen.php';
     require_once './classes/Kisa.php';
     require_once './classes/Sarja.php';
+    
+    //
+    // Joonas: tama sivu on tulosseuranta yksittaiselle sarjalle, joka kuuluu kisaan kisa_id, saat toteuttaa taman
+    //
 
     if (!isset($_GET['kisa_id']) || !isset($_GET['sarja_id'])) {
         echo '<div style="margin-top: 120px; text-align: center;"><h1>Error:</h1> <p>kisa tai sarja id parametri puuttuu</p><br> <a class="btn btn-primary" href="../sovellus"> < Takaisin</a><br><br></div>';
